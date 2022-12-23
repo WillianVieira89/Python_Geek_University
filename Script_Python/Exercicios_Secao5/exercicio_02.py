5f5f4dfba0d5011848cd9ff9b2c70e9c204d054d
Willian Leonardo Vieira <wilianleonardo@hotmail.com>
2022-12-23 02:37:38 -0300

    .
    .

--- Script_Python/Exercicios_Secao5/exercicio_02.py
+++ Script_Python/Exercicios_Secao5/exercicio_02.py
@@ -1,5 +1,4 @@
 from math import sqrt
-
 num = int(input('digite um numero: '))
 
 if num > 0:
--- Script_Python/Exercicios_Secao5/exercicio_03.py
+++ Script_Python/Exercicios_Secao5/exercicio_03.py
@@ -5,7 +5,7 @@ numero = int(input("Digite um numero: "))
 
 if numero >= 0:
     raiz_quad = sqrt(numero)
-    exponencial = pow(numero, 2)
+    exponencial = pow(numero, 4)
     print(f"A Raiz Quadrada do numero digitado é {raiz_quad:.0f}, e quadrado do numero é {exponencial:.0f}")
 
 else:
