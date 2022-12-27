8d3264945bac058ea5b909f9a852804200395e39
Willian Leonardo Vieira <wilianleonardo@hotmail.com>
2022-12-27 20:43:09 -0300

    escopo_variaveis

---
+++ Script_Python/Estudos/escopo_de_variaveis.py
@@ -0,0 +1,19 @@
+"""
+Escopo de variaveis
+
+Dois casos de escopo:
+
+1 - Variaveis globais: São reconhecidas , ou seja, seu escopo compreende, todo o programa.
+
+2 - Variaveis locais: São reconhecidas apenas no bloco onde foram declaradas, ou seja, seu escopo está limitado
+ao bloco onde foi declarada.
+
+Para declarar variaveis em Python fazemos:
+
+nome_da_variavel = valor_da_variavel
+
+Python é uma linguagem de tipagem dinamica. Isso significa que
+ao declararmos uma variavel, nos  não colocamos o tipo de dado dela.
+este tipo é inferido atribuirmos o valor a mesma.
+
+"""
