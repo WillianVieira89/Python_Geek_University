353bb41513f638c3cc4793c50ac09220dca915c4
Willian Leonardo Vieira <wilianleonardo@hotmail.com>
2023-01-24 14:58:26 -0300

    docstrings

---
+++ Script_Python/Estudos/Funções/docstrings.py
@@ -0,0 +1,25 @@
+"""
+Documentando funções com Docstrings
+
+OBS: Podemos ter acesso á documentação de uma função em Python
+utilizando a propriedade especial __doc__
+
+Podemos acessar ainda a documentação com a função help
+
+"""
+
+# Exemplos
+
+def diz_oi():
+    """uma função simples que retorna a string 'Oi' """
+    return 'Oi'
+
+
+def exponencial(numero, potencia=2):
+    """
+    Função que retorna por padrão o quadrado de 'numero' ou 'numero' á 'potência' informada
+    :param numero: Número que desejamos gerar o exponencial
+    :param potencia: Potência que queremos gerar o exponencial. Por padrão é 2
+    :return: Retorna o exponencial de 'numero' por 'potencia'
+    """
+    return numero ** potencia
